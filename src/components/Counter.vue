fba3951cb4c52d6c9517e62cd52f1b2c7a57fd60
Jona <jonathanelian64@gmail.com>
2022-10-31 15:33:56 -0600

    Inicio astro-example

---
+++ src/components/Counter.vue
@@ -0,0 +1,83 @@
+
+<template>
+  <h1>Hola Vuejs</h1>
+
+  <div class="container">
+    <h2 class="count">{{ count }}</h2>
+  </div>
+  <br>
+  <button @click="increase" class="btn-custom" >+1</button>
+  <button @click="decrease" class="btn-custom" >-1</button>
+  <div class="container">
+    <a href="/">Home</a>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+
+    const count = ref(1)
+
+
+    return {
+      count,
+      increase: () => count.value++,
+      decrease: () => count.value--
+    }
+  }
+}
+</script>
+
+<style>
+
+html {
+  font-family: system-ui, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.container {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+
+ a {
+        padding: 10px;
+        color: #000;
+        text-decoration: none;
+        border: 1px solid #000;
+        transition: all 0.3s;
+    }
+
+    a:hover {
+        background-color: cornflowerblue;
+        border: 1px solid #0000;
+    }
+
+.count {
+  align-self: center;
+  justify-self: center;
+  margin: auto;
+}
+
+.btn-custom {
+  padding: 10px;
+  color: #000;
+  text-decoration: none;
+  border: 1px solid #000;
+  transition: all 0.3s;
+  margin: 20px;
+}
+
+.btn-custom:hover {
+  background-color: aquamarine;
+  border: 1px solid #0000;
+}
+
+</style>
+
---
+++ src/pages/astro.md
@@ -0,0 +1,10 @@
+---
+layout: '../layouts/BlogPostLayout.astro'
+title: 'Mi post de Rust'
+author: 'Jona'
+description: 'Un post sobre Rust'
+draft: true
+---
+
+# My post 
+
